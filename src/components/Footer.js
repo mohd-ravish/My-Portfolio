ff45cbb5412a54a2d401c5d8c9e353e7ef3ba071
MOHD RAVISH <mohdravish2003@gmail.com>
2023-07-15 01:27:45 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { Container, Row, Col } from "react-bootstrap";
+// import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  var date = new Date();
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          {/* <MailchimpForm /> */}
+          <Col size={12} sm={4}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={4}>
+
+          </Col>
+          <Col size={12} sm={4} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://linkedin.com/in/mohdravish"><img src={navIcon1} alt="Icon" /></a>
+              {/* <a href="#"><img src={navIcon2} alt="Icon" /></a> */}
+              <a href="https://instagram.com/rrravishh"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright ⓒ {date.getFullYear()}. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,106 @@
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
+
+export const Skills = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
+  return (
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Languages and Tools</h2>
+              {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p> */}
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                <div className="item">
+                  <a href="https://www.cprogramming.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" alt="c" width="40" height="40" /> </a>
+                  <h5>C Programming</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.w3schools.com/cpp/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" alt="cplusplus" width="40" height="40" /> </a>
+                  <h5>C++ Programming</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40" /> </a>
+                  <h5>Python</h5>
+                </div>
+                <div className="item">
+                  <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40" /> </a>
+                  <h5>JavaScript</h5>
+                </div>
+                <div className="item">
+                  <a href="https://kotlinlang.org" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg" alt="kotlin" width="40" height="40" /> </a>
+                  <h5>Kotlin</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40" /> </a>
+                  <h5>HTML</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40" /> </a>
+                  <h5>CSS</h5>
+                </div>
+                <div className="item">
+                  <a href="https://getbootstrap.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg" alt="bootstrap" width="40" height="40" /> </a>
+                  <h5>Bootstrap</h5>
+                </div>
+                <div className="item">
+                  <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40" /> </a>
+                  <h5>ReactJs</h5>
+                </div><div className="item">
+                  <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40" /> </a>
+                  <h5>NodeJs</h5>
+                </div>
+                <div className="item">
+                  <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40" /> </a>
+                  <h5>ExpressJs</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg" alt="mongodb" width="40" height="40" /> </a>
+                  <h5>MongoDB</h5>
+                </div>
+                <div className="item">
+                  <a href="https://www.mysql.com/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40" /> </a>
+                  <h5>MySql</h5>
+                </div>
+                <div className="item">
+                  <a href="https://git-scm.com/" target="_blank" rel="noreferrer"> <img src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg" alt="git" width="40" height="40" /> </a>
+                  <h5>Git</h5>
+                </div>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
+    </section>
+  )
+}
+
+
+
