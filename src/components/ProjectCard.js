3afe73520e66c9ca410cceaff633d724af88bb0d
MOHD RAVISH <mohdravish2003@gmail.com>
2023-07-16 00:51:43 +0530

    Update Projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,10 +4,10 @@ export const ProjectCard = ({ title, description, imgUrl, gitLink }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
-      <a href={gitLink} target="_blank"  rel="noreferrer"><img src={imgUrl} alt="project"/></a>
+        <img src={imgUrl} alt="project" />
         <div className="proj-txtx">
           <h4>{title}</h4>
-          <span>{description}</span>
+          <a className="project-link" href={gitLink} target="_blank" rel="noreferrer"><span>{description}</span></a>
         </div>
       </div>
     </Col>
